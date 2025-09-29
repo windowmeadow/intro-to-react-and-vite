b84b6e61a50ca6e4ee47185f0993cc91b98e0629
Isaiah L <Isaiah@Isaiahs-MacBook-Pro.local>
2025-09-29 19:16:07 -0400

    Changed stuff for boba

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
     const [count, setCount] = useState(0);
-    
+
     return (
         <>
             <div>
@@ -20,10 +20,12 @@ function App() {
                     />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1>Vite + React?
+                What about the BOBA??
+            </h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
+                <button onClick={() => setCount((count) => count + 4)}>
+                    Bobas drank is: {count}
                 </button>
                 <p>
                     Edit <code>src/App.jsx</code> and save to test HMR
@@ -31,6 +33,7 @@ function App() {
             </div>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
+                AND TESTING ONE TWO THREE
             </p>
         </>
     );
